b6b2697eb0c73fbc7beec2f4b4d3519a35502d09
Gui Seek <guiseek@gmail.com>
2020-02-21 13:05:06 -0300

    Angular Scully Start

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { ScullyRoutesService } from '@scullyio/ng-lib';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'uig-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+  links$: Observable<any> = this.scully.available$;
+
+  constructor(private scully: ScullyRoutesService) {}
+
+  ngOnInit(): void {
+    // debug current pages
+    this.links$.subscribe(links => {
+      console.log(links);
+    });
+  }
+
+}
