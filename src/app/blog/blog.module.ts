199873da8503a64221e0b2aa9d3139dcbc45e64e
Gui Seek <guiseek@gmail.com>
2020-02-23 06:32:55 -0300

    blog-post

--- src/app/blog/blog.module.ts
+++ src/app/blog/blog.module.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ScullyLibModule } from '@scullyio/ng-lib';
+import { SharedModule } from './../shared/shared.module';
 import { BlogRoutingModule } from './blog-routing.module';
 import { BlogComponent } from './blog.component';
+import { PostsComponent } from './posts/posts.component';
 
 @NgModule({
-  declarations: [BlogComponent],
-  imports: [CommonModule, BlogRoutingModule, ScullyLibModule],
+  declarations: [BlogComponent, PostsComponent],
+  imports: [CommonModule, SharedModule, BlogRoutingModule, ScullyLibModule],
   // providers: [HighlightService]
 })
 export class BlogModule {}
---
+++ src/app/blog/posts/posts.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ScullyRoute, ScullyRoutesService } from '@scullyio/ng-lib';
+import { Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+
+@Component({
+  selector: 'uig-posts',
+  templateUrl: './posts.component.html',
+  styleUrls: ['./posts.component.scss']
+})
+export class PostsComponent implements OnInit {
+  links$: Observable<ScullyRoute[]>;
+
+  constructor(private scully: ScullyRoutesService) { }
+
+  ngOnInit(): void {
+    // debug current pages
+    this.links$ = this.scully.available$
+      .pipe(
+        map(links => links.filter((link => !!link && link.title))),
+        tap(a => {
+          console.log(a);
+        })
+      );
+  }
+
+}
